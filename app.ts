cd64ab01694f04a6bea872d30ecc74155df77011
Yash Hemnani <yash@gxpress.com>
2026-06-23 17:39:55 +0530

    Added required files for vercel deployment

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+import booksRoutes from './routes/books.routes';
+import { logger } from './utils/logger';
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '2mb' }));
+app.use(pinoHttp({ logger }));
+
+app.use(booksRoutes);
+
+export default app;
--- server.ts
+++ server.ts
@@ -1,17 +1,7 @@
 import 'dotenv/config';
 
-import express from 'express';
-import cors from 'cors';
-import pinoHttp from 'pino-http';
-import booksRoutes from './routes/books.routes';
+import app from './app';
 import { logger } from './utils/logger';
-
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: '2mb' }));
-app.use(pinoHttp({ logger }));
-
-app.use(booksRoutes);
 
 const PORT = process.env.PORT || 3000;
 
